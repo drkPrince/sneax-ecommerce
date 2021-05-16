edff24a0f7a677ef4464c0234631d1b841a136df
Prince <thedrkprns@gmail.com>
2021-05-16 12:57:38 +0530

    added mongodb connector

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 const connectDB = async () => {
 	try {
 		const con = await mongoose.connect(process.env.MONGO_URI, {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true})
-		console.log(`MONGO Connected: ${con.connection.host}`.cyan.underline)
+		console.log(`MONGO Connected: ${con.connection.host}`.green.bold)
 	}
 
 	catch(e) {
