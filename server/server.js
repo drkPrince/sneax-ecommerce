40412ee5bc48d0f5fac425f4e210b4529d172256
Prince <thedrkprns@gmail.com>
2021-06-11 16:20:31 +0530

    fixed paths

--- server/server.js
+++ server/server.js
@@ -55,9 +55,7 @@ if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "../client/build")));
 
 	app.get("*", (req, res) =>
-		res.sendFile(
-			path.resolve(__dirname, "../client", "build", "index.html")
-		)
+		res.sendFile(path.resolve(__dirname, "../client/build/index.html"))
 	);
 }
 
