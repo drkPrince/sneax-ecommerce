b4c8af2947a41b95a37dc94db48e51bdeb09220b
Prince <thedrkprns@gmail.com>
2021-06-11 20:52:35 +0530

    added favicon

--- server/server.js
+++ server/server.js
@@ -50,11 +50,6 @@ app.post("/api/payment", (req, res) => {
 });
 
 if (process.env.NODE_ENV === "production") {
-	// app.use(express.static(path.join(__dirname, "../client/build")));
-	// app.get("*", (req, res) =>
-	// 	res.sendFile(path.resolve(__dirname, "../client/build/index.html"))
-	// );
-
 	app.use(express.static(path.join(__dirname, "/client/build")));
 	app.get("*", (req, res) =>
 		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
